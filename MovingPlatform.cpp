e1eccec15fc43893abbc25d19994e26620f36d9a
upotsu <apeiron365@outlook.jp>
2025-10-26 04:36:47 +0900

    Add files via upload

---
+++ MovingPlatform.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void MyTestFunction(float MyFloatParam,FString MyStringParam)
+{
+	UE_LOG(LogTemp, Display, TEXT("MyFloatParam is %f"),MyFloatParam);
+
+	UE_LOG(LogTemp, Display, TEXT("MyStringParam is %s"), *MyStringParam);
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FString MyName = GetName();
+	MyTestFunction(3.5f,MyName);
+	
+}	 
+
+
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime); //Tick関数の呼び出し
+
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
+	//現在地を呼び出す
+
+	CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+
+	//フレームレートに依存しないコードにする
+
+	SetActorLocation(CurrentLocation);
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+
+	//Rotate the platform
+}
---
+++ MovingPlatform.h
@@ -0,0 +1,34 @@
+	// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
+
+
+	UPROPERTY(EditAnywhere)
+	FVector PlatformVelocity = FVector(0.0f,0.0f,0.0f);
+	//移動プラットフォームの速度と方向を制御するベクトル
+
+};
